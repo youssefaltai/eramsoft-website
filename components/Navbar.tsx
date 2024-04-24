7e6fca18ab093f9d3ecdb674fcc23cf7616e8df5
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-04-24 18:48:22 +0200

    Fix logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import NavLink from "./NavLink";
 
 function Navbar() {
   return <nav className="py-16 w-full px-32 flex justify-between items-center">
-    <Image src="/logo.svg" alt="EramSoft" width={200} height={50} />
+    <Image src="/eramsoft-website/logo.svg" alt="EramSoft" width={200} height={50} />
     <div className="flex gap-8 justify-between items-center">
       <NavLink title="Home" href="#" />
       <NavLink title="Solutions" href="#solutions" />
