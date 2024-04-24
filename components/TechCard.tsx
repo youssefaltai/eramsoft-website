750aabe3acdd479e43e2c45a95e44e4902dadc15
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-04-24 18:52:47 +0200

    fix images

--- components/TechCard.tsx
+++ components/TechCard.tsx
@@ -9,7 +9,7 @@ type TechCardProps = {
 
 function TechCard({ title, image, width, height }: TechCardProps) {
   return <div className="flex flex-col gap-4 justify-center items-center bg-[--primary-color] w-36 h-40 rounded-lg border-2 border-[--secondary-color] px-2">
-    <Image src={`eramsoft-website/${image}`} alt={title} width={width} height={height} />
+    <Image src={`/eramsoft-website${image}`} alt={title} width={width} height={height} />
     <h6 className="font-medium text-base text-center">
       {title}
     </h6>
